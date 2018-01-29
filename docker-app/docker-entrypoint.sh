75eec97eea76487f2877661074fc58b2b9789250
nmakarov <makarovnikolay@itclover.ru>
2018-01-29 11:14:22 +0300

    Fix start script

--- docker-app/docker-entrypoint.sh
+++ docker-app/docker-entrypoint.sh
@@ -3,7 +3,7 @@
 set -e
 
 if [ "$1" = 'start' ]; then
-	env JAVA_TOOL_OPTIONS='-Dfile.encoding=UTF8' sbt http/runMain ru.itclover.streammachine.http.WebServer
+	env JAVA_TOOL_OPTIONS='-Dfile.encoding=UTF8' sbt "http/runMain ru.itclover.streammachine.http.WebServer"
 
 else
   exec "$@"
