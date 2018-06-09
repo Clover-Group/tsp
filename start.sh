db8662c66f86bbfc25f1a79c24822651d254f1ae
Albert Bikeev <albert.bikeev@gmail.com>
2018-06-09 16:23:48 +0300

    Pass env variables to start sh opts

--- start.sh
+++ start.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-env JAVA_OPTS="-Xms2G -Xmx14G" JAVA_TOOL_OPTIONS='-Dfile.encoding=UTF8' sbt "http/runMain ru.itclover.streammachine.http.Launcher"
+
+env JAVA_OPTS=${SM_JAVA_OPTS:-"-Xms2G -Xmx12G"} \
+    JAVA_TOOL_OPTIONS=${SM_TOOL_OPTS:-'-Dfile.encoding=UTF8'} \
+    sbt "http/runMain ru.itclover.streammachine.http.TestLauncher"
