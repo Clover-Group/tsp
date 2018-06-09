7dd782de13815d8df187008b705e5fa4c9d40ba0
Albert Bikeev <albert.bikeev@gmail.com>
2018-06-09 16:30:40 +0300

    Pass env variables to start sh opts

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@
 
 
 env JAVA_OPTS=${SM_JAVA_OPTS:-"-Xms2G -Xmx12G"} \
-    JAVA_TOOL_OPTIONS=${SM_TOOL_OPTS:-'-Dfile.encoding=UTF8'} \
-    sbt "http/runMain ru.itclover.streammachine.http.TestLauncher"
+    JAVA_TOOL_OPTIONS=${SM_JAVA_TOOL_OPTS:-'-Dfile.encoding=UTF8'} \
+    sbt ${SM_LAUNCHER:-"http/runMain ru.itclover.streammachine.http.Launcher"} $SM_LAUNCHER_ARGS
