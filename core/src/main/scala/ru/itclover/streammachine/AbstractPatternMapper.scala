7399b56b1bf2818a306790a817101467c2ae1b85
Albert Bikeev <albert.bikeev@gmail.com>
2018-07-09 18:11:01 +0300

    Rm Segment import

--- core/src/main/scala/ru/itclover/streammachine/AbstractPatternMapper.scala
+++ core/src/main/scala/ru/itclover/streammachine/AbstractPatternMapper.scala
@@ -2,7 +2,6 @@ package ru.itclover.streammachine
 
 import java.time.Instant
 import com.typesafe.scalalogging.Logger
-import ru.itclover.streammachine.aggregators.AggregatorPhases.Segment
 import ru.itclover.streammachine.core.Time.timeOrdering
 import ru.itclover.streammachine.core.{PhaseParser, PhaseResult, Time}
 import ru.itclover.streammachine.core.PhaseResult.{TerminalResult, _}
