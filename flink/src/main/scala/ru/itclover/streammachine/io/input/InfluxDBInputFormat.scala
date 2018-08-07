a222fd697ee2a09e96358b81ef2c513dddcab77b
Albert Bikeev <albert.bikeev@gmail.com>
2018-08-07 18:23:09 +0300

    Add timeouts to InfluxDB source (hard one)

---
+++ flink/src/main/scala/ru/itclover/streammachine/io/input/InfluxDBInputFormat.scala
@@ -0,0 +1,114 @@
+package ru.itclover.streammachine.io.input
+
+import com.google.common.base.Preconditions
+import java.io.IOException
+import java.util
+import java.util.concurrent.TimeUnit
+import java.util.function.Consumer
+import okhttp3.OkHttpClient
+import org.apache.flink.api.common.io.GenericInputFormat
+import org.apache.flink.api.common.io.NonParallelInput
+import org.apache.flink.core.io.GenericInputSplit
+import org.influxdb.InfluxDBFactory
+import org.influxdb.dto.Query
+import org.influxdb.dto.QueryResult
+
+
+object InfluxDBInputFormat {
+  def create() = new Builder()
+
+  class Builder {
+    val obj = new InfluxDBInputFormat()
+
+
+    def url(url: String) = {
+
+      obj.url = url
+      this
+    }
+
+    def username(username: String) = {
+
+      obj.username = username
+      this
+    }
+
+    def password(password: String) = {
+
+      obj.password = password
+      this
+    }
+
+    def database(database: String) = {
+
+      obj.database = database
+      this
+    }
+
+    def timeoutSec(sec: Long) = {
+
+      obj.timeoutSec = sec
+      this
+    }
+
+    def query(query: String) = {
+
+      obj.query = query
+      this
+    }
+
+    def and()= {
+      new Actions()
+    }
+
+    class Actions
+    {
+      def buildIt() = {
+        obj
+      }
+
+      def consumeIt(consumer: Consumer[InfluxDBInputFormat]) = {
+        consumer.accept(buildIt())
+      }
+    }
+  }
+}
+
+@SerialVersionUID(42L)
+class InfluxDBInputFormat extends GenericInputFormat[QueryResult.Result] with NonParallelInput
+{
+  var url: String = _
+  var username: String = _
+  var password: String = _
+  var database: String = _
+  var query: String = _
+  var timeoutSec:  Long = _
+
+  var queryResult: util.ListIterator[QueryResult.Result] = _
+
+  // --------------------------------------------------------------------------
+
+
+  override def open(split: GenericInputSplit) = {
+    val extraConf = new OkHttpClient.Builder()
+      .readTimeout(timeoutSec, TimeUnit.SECONDS)
+      .writeTimeout(timeoutSec, TimeUnit.SECONDS)
+      .connectTimeout(timeoutSec, TimeUnit.SECONDS)
+    val connection = InfluxDBFactory.connect(url, username, password, extraConf)
+    val queryResult = connection.query(new Query(this.query, database))
+    if (queryResult.hasError) {
+      throw new IOException("Could not execute query: " + queryResult.getError)
+    }
+    this.queryResult = queryResult.getResults.listIterator()
+  }
+
+  override def reachedEnd() = !queryResult.hasNext
+
+
+  def nextRecord(reuse: QueryResult.Result): QueryResult.Result = {
+    val result = queryResult.next()
+    reuse.setError(result.getError)
+    reuse.setSeries(result.getSeries)
+    reuse
+  }
+}
