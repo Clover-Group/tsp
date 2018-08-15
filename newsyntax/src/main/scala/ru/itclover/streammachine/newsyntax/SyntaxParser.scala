cb52b33019adccc09324ff70e58bd826f0b04ec0
trolley813 <galanin.dmitrij@clover.global>
2018-08-15 10:37:21 +0300

    (PLTB-1061) some variables renamed

--- newsyntax/src/main/scala/ru/itclover/streammachine/newsyntax/SyntaxParser.scala
+++ newsyntax/src/main/scala/ru/itclover/streammachine/newsyntax/SyntaxParser.scala
@@ -224,8 +224,8 @@ class SyntaxParser[Event](val input: ParserInput)(implicit val timeExtractor: Ti
   def repetitionRange: Rule1[NumericRange[Long]] = rule {
     ("<" ~ ws ~ repetition ~> ((t: Long) => 0L until t)
       | "<=" ~ ws ~ repetition ~> ((t: Long) => 0L to t)
-      | ">" ~ ws ~ repetition ~> ((t: Long) => t + 1 to Long.MaxValue)
-      | ">=" ~ ws ~ repetition ~> ((t: Long) => t to Long.MaxValue)
+      | ">" ~ ws ~ repetition ~> ((t: Long) => t + 1 to Int.MaxValue.toLong)
+      | ">=" ~ ws ~ repetition ~> ((t: Long) => t to Int.MaxValue.toLong)
       | integer ~ ignoreCase("to") ~ ws ~ repetition ~>
       ((t1: OneRowPhaseParser[Event, Long], t2: Long) => t1.extract(null.asInstanceOf[Event]) to t2)
       )
@@ -274,21 +274,22 @@ class SyntaxParser[Event](val input: ParserInput)(implicit val timeExtractor: Ti
       identifier ~ ws ~ "(" ~ ws ~ expr.*(ws ~ "," ~ ws) ~ optional(";" ~ ws ~ underscoreConstraint) ~ ws ~ ")" ~ ws ~>
         ((i: SymbolParser[Event], arguments: Seq[AnyNumericPhaseParser], constraint: Option[Double => Boolean]) => {
           val function = i.symbol.toString.tail.toLowerCase
-          val c: Double => Boolean = constraint.getOrElse(_ => true)
-          //println((1.0 to 10.0 by 1.0).map(q => (q, c(q))))
+          val cond: Double => Boolean = constraint.getOrElse(_ => true)
           function match {
             case "lag" => PhaseParser.Functions.lag(arguments.head).asInstanceOf[AnyNumericPhaseParser]
             case "abs" => PhaseParser.Functions.abs(arguments.head).asInstanceOf[AnyNumericPhaseParser]
             case "minof" =>
               val as = arguments
-              Reduce[Event, Any](TestFunctions.min(_, _, c))(OneRowPhaseParser[Event, Double](_ => Double.MaxValue).asInstanceOf[AnyNumericPhaseParser], as: _*).asInstanceOf[AnyNumericPhaseParser]
+              Reduce[Event, Any](TestFunctions.min(_, _, cond))(OneRowPhaseParser[Event, Double](_ => Double.MaxValue).asInstanceOf[AnyNumericPhaseParser], as: _*).asInstanceOf[AnyNumericPhaseParser]
             case "maxof" =>
               val as = arguments
-              Reduce[Event, Any](TestFunctions.max(_, _, c))(OneRowPhaseParser[Event, Double](_ => Double.MinValue).asInstanceOf[AnyNumericPhaseParser], as: _*).asInstanceOf[AnyNumericPhaseParser]
+              Reduce[Event, Any](TestFunctions.max(_, _, cond))(OneRowPhaseParser[Event, Double](_ => Double.MinValue).asInstanceOf[AnyNumericPhaseParser], as: _*).asInstanceOf[AnyNumericPhaseParser]
             case "avgof" =>
               val as = arguments
-              (Reduce[Event, Any](TestFunctions.plus(_, _, c))(OneRowPhaseParser[Event, Double](_ => 0.0).asInstanceOf[AnyNumericPhaseParser], as: _*) div
-                Reduce[Event, Any](TestFunctions.countNotNan(_, _, c))(OneRowPhaseParser[Event, Double](_ => 0.0).asInstanceOf[AnyNumericPhaseParser], as: _*)).asInstanceOf[AnyNumericPhaseParser]
+              (Reduce[Event, Any](TestFunctions.plus(_, _, cond))(OneRowPhaseParser[Event, Double](_ => 0.0).asInstanceOf[AnyNumericPhaseParser], as: _*) div
+                Reduce[Event, Any](TestFunctions.countNotNan(_, _, cond))(OneRowPhaseParser[Event, Double](_ => 0.0).asInstanceOf[AnyNumericPhaseParser], as: _*)).asInstanceOf[AnyNumericPhaseParser]
+            case "countof" =>
+              Reduce[Event, Any](TestFunctions.countNotNan(_, _, cond))(OneRowPhaseParser[Event, Double](_ => Double.MinValue).asInstanceOf[AnyNumericPhaseParser], as: _*).asInstanceOf[AnyNumericPhaseParser]
             case _ => throw new RuntimeException(s"Unknown function `$function`")
           }
         })
@@ -315,7 +316,7 @@ class SyntaxParser[Event](val input: ParserInput)(implicit val timeExtractor: Ti
 
   def string: Rule1[OneRowPhaseParser[Event, String]] = rule {
     "'" ~ capture(oneOrMore(noneOf("'") | "''")) ~ "'" ~ ws ~>
-      ((id: String) => OneRowPhaseParser[Event, String](_ => id.replace("''", "'")))
+      ((str: String) => OneRowPhaseParser[Event, String](_ => str.replace("''", "'")))
   }
 
   def boolean: Rule1[OneRowPhaseParser[Event, Boolean]] = rule {
