92d1103170fb5d9e325c1be6426399a7fff0c559
kell18 <bikeev.albert@itclover.ru>
2018-09-21 15:25:39 +0300

    (PLTB-1245) Fix jar file path (version from git)

--- start-jar-cluster.sh
+++ start-jar-cluster.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-version=`cat VERSION`
+# Get last tag-version OR parse last untagged version, for example - 0.11.2-4-g1099d94-SNAPSHOT,
+# and increase minor version - 0.11.3-4-g1099d94-SNAPSHOT
+version=`git describe --tags --dirty="-SNAPSHOT" | \
+         awk 'match($0, /\.([0-9]+)\-/){ \
+                print substr($0, 2, RSTART-1) \
+                int(substr($0, RSTART+1, RLENGTH-2))+1 \
+                substr($0, RSTART+RLENGTH-1)\
+              } \
+              !match($0, /\.([0-9]+)\-/) {print substr($0, 2)}'`
 defaultJarPath="./mainRunner/target/scala-2.11/TSP_v${version}.jar"
 
-java ${TSP_JAVA_OPTS:--Xms1G -Xmx6G} \
+echo ${TSP_JAVA_OPTS:--Xms1G -Xmx6G} \
      ${TSP_JAVA_TOOL_OPTS:--Dfile.encoding=UTF8} \
      -cp "${TSP_JAR:-$defaultJarPath}" \
          ${TSP_MAIN_CLASS:-ru.itclover.tsp.http.Launcher} flink-cluster $TSP_LAUNCHER_ARGS
