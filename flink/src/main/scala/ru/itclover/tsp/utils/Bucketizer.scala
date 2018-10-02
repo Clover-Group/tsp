7a0078fe7661d63e6aeb9667669ba39436b929c7
kell18 <bikeev.albert@clover.global>
2018-10-02 19:21:15 +0300

    (PLTB-1327) Bucketizer tests

--- flink/src/main/scala/ru/itclover/tsp/utils/Bucketizer.scala
+++ flink/src/main/scala/ru/itclover/tsp/utils/Bucketizer.scala
@@ -7,11 +7,12 @@ object Bucketizer {
   case class Bucket[T](totalWeight: Long, items: Seq[T])
 
   def bucketizeByWeight[T: WeightExtractor](items: Seq[T], numBuckets: Int): Vector[Bucket[T]] = {
+    require(numBuckets > 0, s"Cannot bucketize to $numBuckets buckets")
     val initBuckets = Vector.fill(numBuckets)(Bucket[T](0, List.empty))
     val bigToSmallItems = items.sortBy(implicitly[WeightExtractor[T]].apply(_)).reverse
     bigToSmallItems.foldLeft(initBuckets) {
       case (buckets, item) => {
-        // TODO OPTIMIZE try to use min-heap here to retrieve min bucket here
+        // TODO OPTIMIZE try to use min-heap here to retrieve min bucket
         val minBucketInd = buckets.zipWithIndex.minBy(_._1.totalWeight)._2
         val minBucket = buckets(minBucketInd)
         val windSize = implicitly[WeightExtractor[T]].apply(item)
--- flink/src/test/scala/ru/itclover/tsp/utils/BucketizerTest.scala
+++ flink/src/test/scala/ru/itclover/tsp/utils/BucketizerTest.scala
@@ -1,4 +1,3 @@
-/*
 package ru.itclover.tsp.utils
 
 import org.scalatest.WordSpec
@@ -19,20 +18,36 @@ class BucketizerTest extends WordSpec with ParserMatchers {
       val simplestResult = Bucketizer.bucketizeByWeight(simplest, 2)
       simplestResult.length shouldBe 2
       simplestResult.map(_.totalWeight).sorted shouldBe Vector(11L, 100L)
-      simplestResult.map(_.items.length).sorted shouldBe Vector(1, 4)
+      simplestResult.map(_.totalWeight).sum shouldBe simplest.sum
 
-      // ..
-      /*val bigger = Seq(0, 1, 2, 3, 0, 100, 5, 10000, 20, 13, 10, 0, 0).map(_.toLong)
-      val biggerResults = Bucketizer.bucketizeByWeight(simplest, 2)
-      biggerResults.length shouldBe 2
-      biggerResults.map(_.totalWeight).sorted shouldBe Vector(11L, 100L)
-      biggerResults.map(_.items.length).sorted shouldBe Vector(1, 4)*/
+      val bigger = Seq(0, 1, 2, 3, 0, 100, 5, 10000, 20, 13, 10, 0, 0).map(_.toLong)
+      val biggerResults = Bucketizer.bucketizeByWeight(bigger, 3)
+      biggerResults.length shouldBe 3
+      biggerResults.map(_.totalWeight).sorted shouldBe Vector(54L, 100L, 10000)
+      biggerResults.map(_.totalWeight).sum shouldBe bigger.sum
     }
 
-    /*"not break on corner cases" in {
-      // ..
-    }*/
+    "not break on corner cases" in {
+      val empty = Seq.empty[Long]
+      val emptyResult = Bucketizer.bucketizeByWeight(empty, 2)
+      emptyResult.length shouldBe 2
+      emptyResult.map(_.totalWeight).sum shouldBe 0L
+
+      val same = Seq(2, 2, 2, 2, 2).map(_.toLong)
+      val sameResult = Bucketizer.bucketizeByWeight(same, 2)
+      sameResult.length shouldBe 2
+      sameResult.map(_.totalWeight).sorted shouldBe Vector(4L, 6L)
+      sameResult.map(_.totalWeight).sum shouldBe same.sum
+
+      val one = Seq(2, 2, 2, 2, 2).map(_.toLong)
+      val oneResult = Bucketizer.bucketizeByWeight(one, 1)
+      oneResult.length shouldBe 1
+      oneResult.map(_.totalWeight).sorted shouldBe Vector(10L)
+      oneResult.map(_.totalWeight).sum shouldBe one.sum
+
+      val zero = Seq(2, 2, 2, 2, 2).map(_.toLong)
+      an [IllegalArgumentException] shouldBe thrownBy(Bucketizer.bucketizeByWeight(zero, 0))
+    }
   }
 
 }
-*/
