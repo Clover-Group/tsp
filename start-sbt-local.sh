489c2c85aa7f910b0da6886ce2c77c766944f1fe
kell18 <bikeev.albert@clover.global>
2018-10-17 14:29:46 +0300

    Fix local sbt starting script

--- start-sbt-local.sh
+++ start-sbt-local.sh
@@ -3,4 +3,4 @@
 
 env JAVA_OPTS="${TSP_JAVA_OPTS:--Xms2G -Xmx4G}" \
     JAVA_TOOL_OPTIONS="${TSP_JAVA_TOOL_OPTS:--Dfile.encoding=UTF8}" \
-    sbt "${TSP_LAUNCHER:-http/runMain ru.itclover.tsp.http.Launcher}" $TSP_LAUNCHER_ARGS
+    sbt "http/runMain ${TSP_LAUNCHER:-ru.itclover.tsp.http.Launcher} $TSP_LAUNCHER_ARGS"
