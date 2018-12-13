ed6e89e3d8f025b57f168033ae3313c11b91cdc8
kell18 <bikeev.albert@clover.global>
2018-12-13 18:42:18 +0300

    Fix routes

--- http/src/main/scala/ru/itclover/tsp/http/routes/JobsRoutes.scala
+++ http/src/main/scala/ru/itclover/tsp/http/routes/JobsRoutes.scala
@@ -130,15 +130,7 @@ trait JobsRoutes extends RoutesProtocols {
       case Right(Some(execResult)) => {
         // todo query read and written rows (onComplete(monitoring.queryJobInfo(request.uuid)))
         val execTime = execResult.getNetRuntime(TimeUnit.SECONDS)
-        onSuccess(monitoring.queryJobAllMetrics(uuid)) {
-          case Right(metrics) =>
-            log.warn(metrics.mkString(";"))
-            val extraMetrics: Map[String, Option[Long]] = metrics
-              .map(kv => kv._1 -> Try(kv._2.toLong).toOption)
-              .filter(kv => kv._1.contains("PatternStats"))
-            complete(SuccessfulResponse(ExecInfo(execTime, extraMetrics)))
-          case Left(err) => complete(InternalServerError, FailureResponse(err))
-        }
+        complete(SuccessfulResponse(ExecInfo(execTime, Map.empty)))
       }
     }
 
