8b4cc1c8f7b8916891387327ff5d130ce7fa961d
Kuzma Leshakov <leshakov.kuzma@clover.global>
2019-08-19 09:09:58 +0300

    Add third test unmatch

--- core/src/test/scala/ru/itclover/tsp/core/TimeSeriesGeneratorTestCase.scala
+++ core/src/test/scala/ru/itclover/tsp/core/TimeSeriesGeneratorTestCase.scala
@@ -1,17 +1,16 @@
 package ru.itclover.tsp.core
 
-import org.scalatest.{Matchers, WordSpec}
-import java.time.{Instant, Duration => JavaDuration}
-
-import scala.concurrent.duration.Duration
 import java.time.temporal.ChronoUnit
+import java.time.{Instant, Duration => JavaDuration}
 import java.util.Random
 
 import cats.Id
-import ru.itclover.tsp.core.Common.{EInt, extractor, timeExtractor}
+import org.scalatest.{Matchers, WordSpec}
+import ru.itclover.tsp.core.Common.{EInt, extractor}
 import ru.itclover.tsp.utils.{Change, Constant, RandomInRange, Timer}
 
 import scala.collection.mutable.ArrayBuffer
+import scala.concurrent.duration.Duration
 import scala.language.reflectiveCalls
 
 class TimeSeriesGeneratorTestCase extends WordSpec with Matchers {
@@ -113,6 +112,48 @@ class TimeSeriesGeneratorTestCase extends WordSpec with Matchers {
       println(result)
 
       true shouldBe true
+    }
+
+    "not to match" in {
+      val patterns = new ArrayBuffer[SimplePattern[EInt, Int]]()
+
+      val events = (
+        for (time <- Timer(from = Instant.now());
+             pump <- RandomInRange(1, 100)(random).map(_.toDouble).timed(
+               Duration.fromNanos(
+                 JavaDuration.of(40, ChronoUnit.SECONDS).toNanos
+               )
+             )
+               .after(Constant(0));
+             speed <- Constant(250d).timed(
+               Duration.fromNanos(
+                 JavaDuration.of(1, ChronoUnit.SECONDS).toNanos
+               )
+             )
+               .after(
+                 Change(
+                   from = 250.0,
+                   to = 0.0,
+                   howLong = Duration.fromNanos(
+                     JavaDuration.of(10, ChronoUnit.SECONDS).toNanos
+                   )
+                 )
+               )
+               .after(Constant(0.0))
+             ) yield Event[Int](time.getEpochSecond, speed.toInt, pump.toInt)).run(seconds = 100)
+
+      events
+        .foreach(
+          event => patterns.append(new SimplePattern[EInt, Int](_ => Result.succ(process(event).toInt))(extractor))
+        )
+
+      val result = (patterns, events).zipped.map { (p, e) =>
+        StateMachine[Id].run(p, Seq(e), p.initialState())
+      }
+
+      println(result)
+
+      false shouldBe false
     }
 
   }
