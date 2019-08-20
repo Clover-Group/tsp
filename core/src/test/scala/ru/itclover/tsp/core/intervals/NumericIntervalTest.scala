cc47cd13f11b414ca5b1bdc88b347f40ef7c9eab
Andrey Romanov <steampart@gmail.com>
2019-08-20 16:25:10 +0300

    add tests for intervals

---
+++ core/src/test/scala/ru/itclover/tsp/core/intervals/NumericIntervalTest.scala
@@ -0,0 +1,44 @@
+package ru.itclover.tsp.core.intervals
+
+import org.scalatest.{Matchers, WordSpec}
+import ru.itclover.tsp.core.Intervals.{Inside, NumericInterval}
+
+/**
+  * Test class for numeric interval
+  */
+class NumericIntervalTest extends WordSpec with Matchers {
+
+  "numeric interval" should {
+
+    val testNumericInterval = NumericInterval[Int](
+      start = 1000,
+      end = Some(4000)
+    )
+
+    "check for containing value" in {
+
+      assertResult(expected = true) {
+        testNumericInterval.contains(2000)
+      }
+
+      assertResult(expected = false) {
+        testNumericInterval.contains(100)
+      }
+
+    }
+
+    "check if infinite" in {
+
+      assertResult(expected = false) {
+        testNumericInterval.isInfinite
+      }
+
+    }
+
+    "retrieve relative position" in {
+      testNumericInterval.getRelativePosition(item = 2500) shouldBe Inside
+    }
+
+  }
+
+}
