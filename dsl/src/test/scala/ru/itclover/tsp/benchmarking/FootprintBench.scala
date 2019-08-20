6ede2311ae8ec1706a8bf3846912c8fb2e3ab793
Andrey Romanov <steampart@gmail.com>
2019-08-20 11:37:17 +0300

    refactored benchmark tests in dsl

--- dsl/src/test/scala/ru/itclover/tsp/benchmarking/FootprintBench.scala
+++ dsl/src/test/scala/ru/itclover/tsp/benchmarking/FootprintBench.scala
@@ -2,11 +2,8 @@ package ru.itclover.tsp.benchmarking
 
 import cats._
 import org.scalatest.{FlatSpec, Matchers}
-import ru.itclover.tsp.core.{Time, Window}
 import ru.itclover.tsp.dsl.v2.{ASTPatternGenerator, TestEvents}
-import ru.itclover.tsp.core.Pattern.IdxExtractor
 import ru.itclover.tsp.core._
-import ru.itclover.tsp.core.aggregators.TimerPattern
 
 import scala.collection.mutable.ArrayBuffer
 import scala.language.reflectiveCalls
@@ -31,35 +28,26 @@ class FootprintBench extends FlatSpec with Matchers {
     val collect = new ArrayBuffer[Long](events.size)
     val actState = sm.run(pattern, events, initialState, (x: IdxValue[T]) => collect += x.index, 1000)
     (System.nanoTime() - start) / 1000000
-
-//    println(org.openjdk.jol.info.GraphLayout.parseInstance(actState).toFootprint)
   }
 
   def repeat[T, S <: PState[T, S]](times: Int, amount: Int, pattern: Pattern[TestEvent, S, T]): Long = {
-    val events = (1 to amount).map(l => TestEvent(l.toLong * 1000, 1, 1, true, 1.0, 2.0)).seq
-    val ts = (1 to times).map(_ => { val t = process(pattern, events); println(t); t }).sum
+    val events = (1 to amount).map(l => TestEvent(l.toLong * 1000, 1, 1, boolSensor = true, 1.0, 2.0)).seq
+    val ts = (1 to times).map(_ => { val t = process(pattern, events); t }).sum
     ts / times
   }
 
-  it should "process ConstPattern correctly" in {
-
-    implicit val patterns = new Patterns[TestEvent] {}
-    import patterns._
+  it should "process pattern by ASTPatternGenerator correctly" in {
 
     val gen = new ASTPatternGenerator[TestEvent, Symbol, Any]
-    val patternString = gen.build("intSensor > 0 for 720 sec", 0.0, fieldsClasses).right.get._1
-    println("String pattern: " + repeat(5, 1000000, patternString))
-//
-//    val patternCouple = timer(patterns.assert(field(e => e.intSensor).gt(const(0))), Window(720000))
-//    println("Couple pattern: " + repeat(10, 10000000, patternCouple) )
-//
-//    val patternMap = timer(patterns.assert(field(e => e.intSensor).map(_ > 0)), Window(720000))
-//    println("Map pattern: " + repeat(10, 10000000, patternMap) )
+    val expectedTime = 3000
 
-//    val success = Result.succ(())
-//    val patternMap2 =
-//      timer(new SimplePattern((e: TestEvent) => if (e.intSensor > 0) success else Result.fail), Window(7200000))
-//    println("Map pattern: " + repeat(10, 10000000, patternMap2) )
+    val patternString = gen.build(
+      "intSensor > 0 for 720 sec",
+      0.0,
+      fieldsClasses
+    ).right.get._1
+    val actualTime = repeat(5, 1000000, patternString)
+    assert(actualTime > expectedTime)
 
   }
 
