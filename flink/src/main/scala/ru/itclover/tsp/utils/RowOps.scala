b7304f255ea23ec6ee396a5594a691a6e1f7b873
trolley813 <galanin.dmitrij@clover.global>
2019-09-27 12:02:10 +0300

    TSP-264 temporary "fix"

--- flink/src/main/scala/ru/itclover/tsp/utils/RowOps.scala
+++ flink/src/main/scala/ru/itclover/tsp/utils/RowOps.scala
@@ -25,6 +25,7 @@ object RowOps {
         case d: java.lang.Double => (d * tsMultiplier).toLong
         case f: java.lang.Float  => (f * tsMultiplier).toLong
         case n: java.lang.Number => (n.doubleValue() * tsMultiplier).toLong
+        case null                => 0L // TODO: Where can nulls come from?
         case x                   => sys.error(s"Cannot parse time `$x` from field $fieldId, should be number of millis since 1.1.1970")
       }
       CoreTime(toMillis = millis)
