18b38148cc1421c75bb840df2da3805e12df2d83
Bulat Fattakhov <fabura@yandex.ru>
2019-11-06 00:43:55 +0100

    Refactor MapPattern

--- core/src/main/scala/ru/itclover/tsp/core/MapPattern.scala
+++ core/src/main/scala/ru/itclover/tsp/core/MapPattern.scala
@@ -2,31 +2,21 @@ package ru.itclover.tsp.core
 import cats.syntax.functor._
 import cats.{Foldable, Functor, Monad}
 import ru.itclover.tsp.core.PQueue.MapPQueue
-import ru.itclover.tsp.core.Pattern.QI
 
 import scala.language.higherKinds
 
-case class MapPattern[Event, T1, T2, InnerState <: PState[T1, InnerState]](inner: Pattern[Event, InnerState, T1])(
-  val func: T1 => Result[T2]
-) extends Pattern[Event, MapPState[InnerState, T1, T2], T2] {
+case class MapPattern[Event, T1, T2, InnerState](inner: Pattern[Event, InnerState, T1])(val func: T1 => Result[T2])
+    extends Pattern[Event, MapPState[InnerState, T1], T2] {
   override def apply[F[_]: Monad, Cont[_]: Foldable: Functor](
-    oldState: MapPState[InnerState, T1, T2],
+    oldState: MapPState[InnerState, T1],
+    oldQueue: PQueue[T2],
     event: Cont[Event]
-  ): F[MapPState[InnerState, T1, T2]] =
-    inner(oldState.innerState, event).map(innerResult => oldState.copy(innerState = innerResult))
+  ): F[(MapPState[InnerState, T1], PQueue[T2])] =
+    inner(oldState.innerState, oldState.innerQueue, event).map {
+      case (innerResult, innerQueue) => MapPState(innerState = innerResult, innerQueue) -> MapPQueue(innerQueue, func)
+    }
 
-  override def initialState(): MapPState[InnerState, T1, T2] = MapPState(innerState = inner.initialState(), func)
+  override def initialState(): MapPState[InnerState, T1] = MapPState(inner.initialState(), PQueue.empty)
 }
 
-case class MapPState[InnerState <: PState[T1, InnerState], T1, T2](
-  innerState: InnerState,
-  func: T1 => Result[T2]
-) extends PState[T2, MapPState[InnerState, T1, T2]] {
-  override def queue: QI[T2] = MapPQueue(innerState.queue, func)
-  override def copyWith(queue: QI[T2]): MapPState[InnerState, T1, T2] = {
-    val prevSize = innerState.queue.size.toLong
-    val toDrop: Long = prevSize - queue.size
-    assert(toDrop >= 0, "Illegal state, queue cannot grow in map")
-    this.copy(innerState = innerState.copyWith(innerState.queue.drop(toDrop)))
-  }
-}
+case class MapPState[InnerState, T1](innerState: InnerState, innerQueue: PQueue[T1])
