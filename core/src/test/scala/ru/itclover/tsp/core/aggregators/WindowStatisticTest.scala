13d4103f1f3be3eec8b696c7cdbc327583c932b6
Андрей Романов <romanov_andrey_vl@protonmail.com>
2019-11-11 10:35:46 +0300

    more tests

--- core/src/test/scala/ru/itclover/tsp/core/aggregators/WindowStatisticTest.scala
+++ core/src/test/scala/ru/itclover/tsp/core/aggregators/WindowStatisticTest.scala
@@ -9,7 +9,7 @@ import ru.itclover.tsp.core.fixtures.Common.EInt
 import ru.itclover.tsp.core.fixtures.Event
 import ru.itclover.tsp.core.utils.TimeSeriesGenerator.Increment
 import ru.itclover.tsp.core.utils.{Constant, Timer}
-import ru.itclover.tsp.core.{IdxValue, Patterns, StateMachine, Succ}
+import ru.itclover.tsp.core.{IdxValue, Patterns, StateMachine, Succ, Time, Window}
 
 import scala.collection.mutable.ArrayBuffer
 import scala.concurrent.duration._
@@ -20,7 +20,7 @@ class WindowStatisticTest extends WordSpec with Matchers {
   val pat = Patterns[EInt]
   import pat._
 
-  "window statistic pattern" should {
+  "Window Statistic Pattern" should {
 
     "count truthMillis" in {
 
@@ -38,6 +38,64 @@ class WindowStatisticTest extends WordSpec with Matchers {
       collect(10) shouldBe IdxValue(10, 10, Succ(10000))
       collect(45) shouldBe IdxValue(45, 45, Succ(5000))
       collect(70) shouldBe IdxValue(70, 70, Succ(0))
+    }
+
+  }
+
+  "Window Statistic Result" should {
+
+    val testResult = WindowStatisticResult(
+      idx = 1000,
+      time = Time(250),
+      lastWasSuccess = false,
+      successCount = 10,
+      successMillis = 1000,
+      failCount = 5,
+      failMillis = 500
+    )
+
+    "retrieve total millis" in {
+      testResult.totalMillis shouldBe 1500
+    }
+
+    "retrieve total count" in {
+      testResult.totalCount shouldBe 15
+    }
+
+    "plus change" in {
+
+      val plusResult = testResult.plusChange(
+        WindowStatisticQueueInstance(
+          100,
+          Time(100),
+          false,
+          150,
+          150
+        ),
+        Window(100)
+      )
+
+      plusResult.failCount shouldBe 6
+      plusResult.failMillis shouldBe 600
+
+    }
+
+    "minus change" in {
+
+      val minusResult = testResult.minusChange(
+        WindowStatisticQueueInstance(
+          5,
+          Time(50),
+          false,
+          300,
+          200
+        ),
+        Window(100)
+      )
+
+      minusResult.failCount shouldBe 4
+      minusResult.failMillis shouldBe 500
+
     }
 
   }
