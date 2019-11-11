7c91259206ab113110f8322ee00cb36bf1e0e828
Андрей Романов <romanov_andrey_vl@protonmail.com>
2019-11-11 11:38:38 +0300

    more tests

--- core/src/test/scala/ru/itclover/tsp/core/patterns/AndThenPatternTest.scala
+++ core/src/test/scala/ru/itclover/tsp/core/patterns/AndThenPatternTest.scala
@@ -3,14 +3,13 @@ package ru.itclover.tsp.core.patterns
 import java.time.Instant
 
 import cats.Id
-import cats.instances.boolean._
 import cats.implicits._
 import org.scalatest.{FlatSpec, Matchers}
 import ru.itclover.tsp.core.fixtures.Common.EInt
 import ru.itclover.tsp.core.fixtures.Event
 import ru.itclover.tsp.core.utils.TimeSeriesGenerator.Increment
 import ru.itclover.tsp.core.utils.{Change, Constant, Timer}
-import ru.itclover.tsp.core.{IdxValue, Patterns, StateMachine, Window}
+import ru.itclover.tsp.core.{IdxValue, Patterns, Result, StateMachine, Window}
 
 import scala.collection.mutable.ArrayBuffer
 import scala.concurrent.duration._
@@ -63,11 +62,19 @@ class AndThenPatternTest extends FlatSpec with Matchers {
   }
 
   it should "parse complex rule" in {
-
-    import cats.instances.boolean._
-    import cats.implicits._
-    //val pattern1 = p.field(_.row > 50).and(p.count(p.field(_.row), Window(100)) > 10)
+    val pattern1 = p.field(_.row > 50).and(p.truthCount(p.assert(p.field(_.row) > p.const(10)), Window(10)) > p.const(0))
     //val pattern2 = p.assert(p.lag(field(_.row > 10), Window(100)))
+
+    val events = (for (time <- Timer(from = Instant.now());
+                       idx  <- Increment;
+                       row  <- Constant(51).timed(10.seconds).after(Constant(10)))
+      yield Event[Int](time.toEpochMilli, idx.toLong, row.toInt, 0)).run(seconds = 11)
+
+    val out = new ArrayBuffer[IdxValue[_]]()
+    StateMachine[Id].run(pattern1, events, pattern1.initialState(), (x: IdxValue[_]) => out += x, 1)
+
+    out.size shouldBe 11
+    out(10) shouldBe IdxValue(10, 10, Result.succ(false))
 
   }
 
