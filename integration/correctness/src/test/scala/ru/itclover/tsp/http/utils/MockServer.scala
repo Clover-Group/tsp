4a28640bbdba46590bdea5fe1bc636396532d336
Андрей Романов <romanov_andrey_vl@protonmail.com>
2019-11-20 09:12:08 +0300

    restored monitoring test

--- integration/correctness/src/test/scala/ru/itclover/tsp/http/utils/MockServer.scala
+++ integration/correctness/src/test/scala/ru/itclover/tsp/http/utils/MockServer.scala
@@ -1,80 +1,80 @@
-//package ru.itclover.tsp.http.utils
-//import akka.Done
-//import akka.actor.ActorSystem
-//import akka.http.scaladsl.model.ContentTypes._
-//import akka.http.scaladsl.model.StatusCodes
-//import akka.http.scaladsl.model.headers.`Content-Type`
-//import akka.http.scaladsl.server.{HttpApp, RejectionHandler, Route}
-//import ru.itclover.tsp.http.services.flink.MonitoringServiceModel._
-//import ru.itclover.tsp.http.services.flink.MonitoringServiceProtocols
-//
-//import scala.concurrent.{ExecutionContext, Future, Promise, blocking}
-//
-//object MockServer extends HttpApp with MonitoringServiceProtocols {
-//  private var shouldStop = false
-//  private var runs = false
-//
-//  def isRunning: Boolean = runs
-//
-//  override def routes: Route =
-//    path("jobs" / "overview"./) {
-//      get {
-//        complete(
-//          JobsOverview(List(JobBrief("1", "job1"), JobBrief("2", "job2")))
-//        )
-//      }
-//    } ~ path("jobs" / "1"./) {
-//      get {
-//        complete(
-//          JobDetails("1", "job1", "FINISHED", 0, 1, Vector(Vertex("101", "v101", VertexMetrics(100, 100, Some(0)))))
-//        )
-//      }
-//    } ~ path("jobs" / "2"./) {
-//      get {
-//        complete(
-//          JobDetails("2", "job2", "FINISHED", 0, 1, Vector(Vertex("201", "v201", VertexMetrics(100, 100, Some(0)))))
-//        )
-//      }
-//    } ~ path("jobs" / "1"./) {
-//      patch {
-//        complete(EmptyResponse())
-//      }
-//    } ~ path("jobs" / "2"./) {
-//      patch {
-//        complete(EmptyResponse())
-//      }
-//    } ~ path("jobs" / "1" / "vertices" / "101" / "metrics") {
-//      parameter('get ? "") { metricList =>
-//        get {
-//          val metrics = metricList.split(",")
-//          if (metrics.isEmpty) {
-//            complete(
-//              List(MetricName("metric1.1"), MetricName("metric1.2"))
-//            )
-//          } else {
-//            complete(
-//              List(Metric("metric1.1", "1001"), Metric("metric1.2", "1002")).filter(m => metrics.contains(m.id))
-//            )
-//          }
-//        }
-//      }
-//    } ~ path("jobs" / "2" / "vertices" / "201" / "metrics") {
-//      parameter('get ? "") { metricList =>
-//        get {
-//          val metrics = metricList.split(",")
-//          if (metrics.isEmpty) {
-//            complete(
-//              List(MetricName("metric2.1"))
-//            )
-//          } else {
-//            complete(
-//              List(Metric("metric2.1", "2001")).filter(m => metrics.contains(m.id))
-//            )
-//          }
-//        }
-//      }
-//    }
-//
+package ru.itclover.tsp.http.utils
+import akka.Done
+import akka.actor.ActorSystem
+import akka.http.scaladsl.model.ContentTypes._
+import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.model.headers.`Content-Type`
+import akka.http.scaladsl.server.{HttpApp, RejectionHandler, Route}
+import ru.itclover.tsp.http.services.flink.MonitoringServiceModel._
+import ru.itclover.tsp.http.services.flink.MonitoringServiceProtocols
+
+import scala.concurrent.{ExecutionContext, Future, Promise, blocking}
+
+object MockServer extends HttpApp with MonitoringServiceProtocols {
+  private var shouldStop = false
+  private var runs = false
+
+  def isRunning: Boolean = runs
+
+  override def routes: Route =
+    path("jobs" / "overview"./) {
+      get {
+        complete(
+          JobsOverview(List(JobBrief("1", "job1"), JobBrief("2", "job2")))
+        )
+      }
+    } ~ path("jobs" / "1"./) {
+      get {
+        complete(
+          JobDetails("1", "job1", "FINISHED", 0, 1, Vector(Vertex("101", "v101", VertexMetrics(100, 100, Some(0)))))
+        )
+      }
+    } ~ path("jobs" / "2"./) {
+      get {
+        complete(
+          JobDetails("2", "job2", "FINISHED", 0, 1, Vector(Vertex("201", "v201", VertexMetrics(100, 100, Some(0)))))
+        )
+      }
+    } ~ path("jobs" / "1"./) {
+      patch {
+        complete(EmptyResponse())
+      }
+    } ~ path("jobs" / "2"./) {
+      patch {
+        complete(EmptyResponse())
+      }
+    } ~ path("jobs" / "1" / "vertices" / "101" / "metrics") {
+      parameter('get ? "") { metricList =>
+        get {
+          val metrics = metricList.split(",")
+          if (metrics.isEmpty) {
+            complete(
+              List(MetricName("metric1.1"), MetricName("metric1.2"))
+            )
+          } else {
+            complete(
+              List(Metric("metric1.1", "1001"), Metric("metric1.2", "1002")).filter(m => metrics.contains(m.id))
+            )
+          }
+        }
+      }
+    } ~ path("jobs" / "2" / "vertices" / "201" / "metrics") {
+      parameter('get ? "") { metricList =>
+        get {
+          val metrics = metricList.split(",")
+          if (metrics.isEmpty) {
+            complete(
+              List(MetricName("metric2.1"))
+            )
+          } else {
+            complete(
+              List(Metric("metric2.1", "2001")).filter(m => metrics.contains(m.id))
+            )
+          }
+        }
+      }
+    }
+
 //  implicit def rejectionsHandler: RejectionHandler = RejectionHandler
 //    .newBuilder()
 //    .handleNotFound {
@@ -83,31 +83,31 @@
 //      }
 //    }
 //    .result()
-//
-//  override def waitForShutdownSignal(
-//    actorSystem: ActorSystem
-//  )(implicit executionContext: ExecutionContext): Future[Done] = {
-//    val promise = Promise[Done]()
-//    sys.addShutdownHook {
-//      runs = false
-//      promise.trySuccess(Done)
-//    }
-//    Future {
-//      blocking {
-//        if (shouldStop) {
-//          runs = false
-//          promise.trySuccess(Done)
-//        }
-//      }
-//    }
-//    runs = false
-//    promise.future
-//  }
-//
-//  override def startServer(host: String, port: Int): Unit = {
-//    runs = true
-//    super.startServer(host, port)
-//  }
-//
-//  def stop(): Unit = { shouldStop = true }
-//}
+
+  override def waitForShutdownSignal(
+    actorSystem: ActorSystem
+  )(implicit executionContext: ExecutionContext): Future[Done] = {
+    val promise = Promise[Done]()
+    sys.addShutdownHook {
+      runs = false
+      promise.trySuccess(Done)
+    }
+    Future {
+      blocking {
+        if (shouldStop) {
+          runs = false
+          promise.trySuccess(Done)
+        }
+      }
+    }
+    runs = false
+    promise.future
+  }
+
+  override def startServer(host: String, port: Int): Unit = {
+    runs = true
+    super.startServer(host, port)
+  }
+
+  def stop(): Unit = { shouldStop = true }
+}
