cfd3960898a2d5ba4f1b96dd532806445c8f4452
trolley813 <trolleybus.1329@gmail.com>
2021-11-26 10:52:28 +0300

    merged

--- core/src/main/scala/ru/itclover/tsp/core/aggregators/WaitPattern.scala
+++ core/src/main/scala/ru/itclover/tsp/core/aggregators/WaitPattern.scala
@@ -60,12 +60,12 @@ case class WaitAccumState[T](windowQueue: m.Queue[(Idx, Time)], lastFail: Boolea
         case (_, t) => t <= end
       }
 
-      val waitStart =
-        if (lastTime._2.toMillis != 0 && Try(outputs.head._2.plus(window) < outputs.last._2).getOrElse(false)) {
-          outputs.headOption
-        } else {
-          Some(lastTime)
-        }
+      val waitStart = if (lastTime._2.toMillis != 0 && Try(outputs.head._2.plus(window) < outputs.last._2).getOrElse(false)) {
+        outputs.headOption
+      } else {
+        Some(cleanedWindowQueue.lastOption.getOrElse(lastTime))
+      }
+
       val waitEnd = outputs.lastOption
 
       val newOptResult = createIdxValue(waitStart, waitEnd, idxValue.value)
