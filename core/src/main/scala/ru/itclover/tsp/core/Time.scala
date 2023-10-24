fa9cd6af8ee8ade9cfec0851950cee98733ac64d
trolley813 <trolleybus.1329@gmail.com>
2023-10-24 10:19:35 +0300

    (TSP-333) fixed JSON datetime serialization

--- core/src/main/scala/ru/itclover/tsp/core/Time.scala
+++ core/src/main/scala/ru/itclover/tsp/core/Time.scala
@@ -23,7 +23,7 @@ object Time {
   }
 
   val DATE_TIME_FORMAT: DateTimeFormatter =
-    DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS").withZone(ZoneId.of("UTC"))
+    DateTimeFormatter.ISO_OFFSET_DATE_TIME.withZone(ZoneId.of("UTC"))
 
   implicit def durationWindow(duration: Duration): Window = Window(toMillis = duration.toMillis)
 
